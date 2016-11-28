94020d542629592a62bd06fecb825c3ad2dcae26
Svett Ralchev <svet@ralch.com>
2016-11-28 15:42:58 +0000

    Update vscode configuration

--- .vim/config/plugin/ctrlp.vim
+++ .vim/config/plugin/ctrlp.vim
@@ -1,11 +1,3 @@
-nnoremap <C-p> :CtrlPCurWD<cr>
-nnoremap <leader>f :CtrlPCurWD<cr>
-nnoremap <leader>m :CtrlPMRUFiles<cr>
-nnoremap <leader>F :CtrlPBufTag<cr>
-nnoremap <leader>S :CtrlPTag<cr>
-nnoremap <leader>L :CtrlPLine<cr>
-nnoremap <leader>b :CtrlPBuffer<cr>
-
 let g:ctrlp_match_window = 'bottom,order:ttb,min:1,max:10,results:50'
 let g:ctrlp_clear_cache_on_exit = 1
 let g:ctrlp_cache_dir='~/.vim/.cache/ctrlp'
@@ -19,3 +11,11 @@ let expr = expr . '|\.dll$\|\.pyc$\|\.svn\|\.hg\|\.bzr\|\.git\|\.sass-cache\'
 let expr = expr . '|\.class\|\.scssc\|Godeps/*'
 let cmd = 'ag %s -U -l --hidden --nocolor -g ""'
 let g:ctrlp_user_command = cmd . ' | grep -v "'.expr .'"'
+
+nnoremap <C-p> :CtrlPCurWD<cr>
+nnoremap <leader>f :CtrlPCurWD<cr>
+nnoremap <leader>m :CtrlPMRUFiles<cr>
+nnoremap <leader>F :CtrlPBufTag<cr>
+nnoremap <leader>S :CtrlPTag<cr>
+nnoremap <leader>L :CtrlPLine<cr>
+nnoremap <leader>b :CtrlPBuffer<cr>
---
+++ .vscode/extensions/lukehoban.Go-0.6.50/out/src/goExtraInfo.js
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------*/
+'use strict';
+var vscode_1 = require('vscode');
+var goDeclaration_1 = require('./goDeclaration');
+var GoHoverProvider = (function () {
+    function GoHoverProvider(toolForDocs) {
+        this.toolForDocs = 'godoc';
+        this.toolForDocs = toolForDocs;
+    }
+    GoHoverProvider.prototype.provideHover = function (document, position, token) {
+        return goDeclaration_1.definitionLocation(document, position, this.toolForDocs, true).then(function (definitionInfo) {
+            if (definitionInfo == null)
+                return null;
+            var lines = definitionInfo.declarationlines
+                .filter(function (line) { return !line.startsWith('\t//') && line !== ''; })
+                .map(function (line) { return line.replace(/\t/g, '    '); });
+            var text;
+            text = lines.join('\n').replace(/\n+$/, '');
+            var hoverTexts = [];
+            hoverTexts.push({ language: 'go', value: text });
+            if (definitionInfo.doc != null) {
+                hoverTexts.push(definitionInfo.doc);
+            }
+            var hover = new vscode_1.Hover(hoverTexts);
+            return hover;
+        });
+    };
+    return GoHoverProvider;
+}());
+exports.GoHoverProvider = GoHoverProvider;
+//# sourceMappingURL=goExtraInfo.js.map
