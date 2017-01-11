acb984dfb6d029d9957c567f6d03455bc9be67fd
Svetlin Ralchev <svet@ralch.com>
2017-01-11 21:26:38 +0000

    Update VSCode Extensions

--- .vscode/extensions/robertohuertasm.vscode-icons-5.0.0/out/src/utils/vscode-extensions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const vscode = require("vscode");
-function getConfig() {
-    return vscode.workspace.getConfiguration();
-}
-exports.getConfig = getConfig;
-//# sourceMappingURL=vscode-extensions.js.map
